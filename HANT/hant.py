48ceaec7d05da31630b82a53cfb446cc9a586c08
Berk Buzcu <berk.buzcu@ozu.edu.tr>
2022-10-13 16:27:01 +0300

    Fix relative import of python2 side

--- HANT/hant.py
+++ HANT/hant.py
@@ -103,7 +103,7 @@ class HANT:
 
     def start_robot_server(self, agent_interaction_type):
         gw = execnet.makegateway(
-            "popen//python=D:\PythonProjects\Human-Robot-Nego\\agent_interaction_models\.venv\Scripts\python.exe")
+            "popen//python=.\\agent_interaction_models\\.venv\\Scripts\\python.exe")
         channel = gw.remote_exec("""
                                     from agent_interaction_models.robot_server import RobotServer
                                     robot_server = RobotServer(channel)
