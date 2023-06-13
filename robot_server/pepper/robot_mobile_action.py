9cf98b59318c0007bf679afcacc581c0d397a883
berkecankocyigit <39878835+berkecankocyigit@users.noreply.github.com>
2023-06-13 16:13:58 +0300

    fix pepper

--- robot_server/pepper/robot_mobile_action.py
+++ robot_server/pepper/robot_mobile_action.py
@@ -10,14 +10,30 @@ class RobotMobileAction(RobotAction):
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
         #<self.managerProxy.runBehavior(self.folder_path + "LetsStart")
         #self.managerProxy.runBehavior("pepper-c91fbb/StandUp")
+        self.tts.say("Hello my name is Pepper! Let's negotiate!")
         self.tts.say("What is your offer?")
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
 
-    def accept(self):
-        self.play_gesture_file("accept")
+
 
     def play_gesture_file(self, gesture_file):
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
-        self.managerProxy.runBehavior(str(self.folder_path + gesture_file + "/behavior_1"))
+        self.animatedSpeechProxy.say(gesture_file)
         self.managerProxy.runBehavior("pepper-c91fbb/StandUp")
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
+    
+    def leave_negotiation(self):
+        self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
+        self.tts.say("Negotiation is over.")
+        self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
+
+    ################################
+    # 		     ACCEPT            #
+    ################################
+
+    def accept(self):
+        self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
+        self.animatedSpeechProxy.say("Great! ^start(pepper-c91fbb/Pepper 2/accept) I accept your offer!")
+        self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
+
+    
