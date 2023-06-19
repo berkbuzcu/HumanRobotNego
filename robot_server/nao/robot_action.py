1b33e130f0790c25944e9be8cc86ed4dc0ec22a0
berkecankocyigit <39878835+berkecankocyigit@users.noreply.github.com>
2023-06-19 20:54:26 +0300

    domain fix

--- robot_server/nao/robot_action.py
+++ robot_server/nao/robot_action.py
@@ -83,7 +83,7 @@ class RobotAction(IRobot):
         self.tts.say(message)
 
     def tell_offer(self, offer):
-        if "duration" in offer.keys():
+        if "duration" in offer.keys() and "season" in offer.keys():
             speak_strings = [
                 "I'd like to visit %s in %s for %s and stay at a %s" % (offer['destination'], offer['season'], offer['duration'], offer['accommodation']),
                 "I want to stay at a %s in %s for %s in %s" % (offer['accommodation'], offer['season'], offer['duration'], offer['destination']),
@@ -91,7 +91,7 @@ class RobotAction(IRobot):
                 "How about visiting %s in %s for %s and stay at a nice %s?" % (offer['destination'], offer['season'], offer['duration'], offer['accommodation']),
                 "I want to spend %s in %s during %s and stay at a %s" % (offer['duration'], offer['destination'], offer['season'], offer['accommodation'])]
         
-        elif "events" in offer.keys():
+        elif "events" in offer.keys() and  "season" in offer.keys():
             activity_to_text = {
                 "shopping": "shopping",
                 "show": "see shows",
@@ -104,7 +104,20 @@ class RobotAction(IRobot):
                 "Let's go to %s this %s to %s and stay at a %s" % (offer['destination'], offer['season'], activity_to_text[offer['events']], offer['accommodation']),
                 "How about visiting %s in %s for %s and stay at a nice %s" % (offer['destination'], offer['season'], activity_to_text[offer['events']], offer['accommodation']),
                 "I want to experience %s in %s during %s and stay at a %s" % (activity_to_text[offer['events']], offer['destination'], offer['season'], offer['accommodation'])]
-
+            
+        elif "duration" in offer.keys() and "events" in offer.keys():
+            activity_to_text = {
+                "shopping": "shopping",
+                "festival": "participating festival",
+                "museum": "visiting museums",
+                "sports": "do sports",
+            }
+            speak_strings = [
+                "I'd like to visit %s for %s for %s and stay at a %s" % (offer['destination'], offer['duration'], activity_to_text[offer['events']], offer['accommodation']),
+                "I want to stay at a %s for %s and %s in %s" % (offer['accommodation'], offer['duration'], activity_to_text[offer['events']], offer['destination']),
+                "Let's go to %s for %s to %s and stay at a %s" % (offer['destination'], offer['duration'], activity_to_text[offer['events']], offer['accommodation']),
+                "How about visiting %s for %s for %s and stay at a nice %s" % (offer['destination'], offer['duration'], activity_to_text[offer['events']], offer['accommodation']),
+                "I want to experience %s in %s for %s and stay at a %s" % (activity_to_text[offer['events']], offer['destination'], offer['duration'], offer['accommodation'])]
         else:
             speak_string = "I give you"
             non_zero_count = 0
