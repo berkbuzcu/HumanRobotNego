715d0589f1da02639b61eed02cfeda2da5c776ab
Berk Buzcu <berk.buzcu@ozu.edu.tr>
2023-07-24 21:29:04 +0300

    Sabanci robots

--- robot_server/pepper/robot_mobile_action.py
+++ robot_server/pepper/robot_mobile_action.py
@@ -7,23 +7,33 @@ class RobotMobileAction(RobotAction):
         super(RobotMobileAction, self).init_robot(self.folder_path)
 
     def greet(self):
+        if self.managerProxy.isBehaviorRunning('standup/behavior_1'):
+            self.managerProxy.stopBehavior('standup/behavior_1')
+            
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
-        #<self.managerProxy.runBehavior(self.folder_path + "LetsStart")
-        #self.managerProxy.runBehavior("pepper-c91fbb/StandUp")
+        self.managerProxy.runBehavior("standup/behavior_1", _async=True)
+        if self.managerProxy.isBehaviorRunning('standup/behavior_1'):
+            self.managerProxy.stopBehavior('standup/behavior_1')
         self.tts.say("Hello my name is Pepper! Let's negotiate!")
         self.tts.say("What is your offer?")
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
 
     def play_gesture_file(self, gesture_file):
+
+            
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
         self.animatedSpeechProxy.say(gesture_file)
-        self.managerProxy.runBehavior("standup/behavior_1")
+        self.managerProxy.runBehavior("standup/behavior_1", _async=True)
+        if self.managerProxy.isBehaviorRunning('standup/behavior_1'):
+            self.managerProxy.stopBehavior('standup/behavior_1')
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
+
     
-    def leave_negotiation(self):
+    def leave_negotiation(self):            
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
         self.tts.say("Negotiation is over.")
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
+
 
     ################################
     # 		     ACCEPT            #
@@ -33,5 +43,3 @@ class RobotMobileAction(RobotAction):
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", False)
         self.animatedSpeechProxy.say("Great! ^start(pepper-c91fbb/Pepper 2/accept) I accept your offer!")
         self.autonomousProxy.setAutonomousAbilityEnabled("BackgroundMovement", True)
-
-    
